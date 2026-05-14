f810cef8d1c4113d54fa469c2e8de8a64f11c48d
Juan Manuel Panozzo Zénere <juanmanuel.panozzozenere@alumnos.uai.edu.ar>
2026-05-14 18:25:46 -0300

    refactor: extract stdinResolution to shared utils module (#12)

--- src/core/pull-flow.ts
+++ src/core/pull-flow.ts
@@ -1,18 +1,6 @@
-import * as readline from "node:readline";
 import type { IGitClient } from "./ports/git-client.port";
 import type { IUI } from "./ports/ui.port";
-
-function stdinResolution(): Promise<boolean> {
-  return new Promise((resolve) => {
-    const rl = readline.createInterface({ input: process.stdin });
-    process.stdin.setRawMode(true);
-    process.stdin.once("data", (key: Buffer) => {
-      rl.close();
-      process.stdin.setRawMode(false);
-      resolve(key[0] !== 0x1b);
-    });
-  });
-}
+import { stdinResolution } from "../utils/stdin-resolution";
 
 export class PullFlow {
   constructor(
