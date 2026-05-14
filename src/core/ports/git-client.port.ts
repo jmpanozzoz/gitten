b05b813f68c4696cab45b1557a61ae1f9dd873e7
Juan Manuel Panozzo Zenere <juanmanuel.panozzozenere@alumnos.uai.edu.ar>
2026-05-14 19:28:12 -0300

    feat: show diff stat summary after staging in sync flow

--- src/core/ports/git-client.port.ts
+++ src/core/ports/git-client.port.ts
@@ -22,6 +22,11 @@ export interface PullResult {
   filesChanged: number;
 }
 
+export interface DiffStat {
+  insertions: number;
+  deletions: number;
+}
+
 export interface IGitClient {
   checkIsRepo(): Promise<boolean>;
   hasIndexLock(): Promise<boolean>;
@@ -45,6 +50,7 @@ export interface IGitClient {
   mergeContinue(): Promise<void>;
   getStatus(): Promise<StatusSummary>;
   addAll(): Promise<void>;
+  getDiffStat(): Promise<DiffStat>;
   commit(message: string): Promise<void>;
   push(setUpstream?: boolean): Promise<void>;
 }
